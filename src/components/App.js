e6d83384622a53296bbfcd10022b5400ccde6f01
Himanshu Gawari <himanshu.gawari@devkraft.in>
2021-03-01 14:47:42 +0530

    added images support, html  and clean plugin

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import '../index.css';
 import Avengers from './Avengers';
+import '../index.css';
+import sword from '../images/sword.svg';
 
 const App = () => {
   return (
     <>
-      <section className='hero'></section>
       <main>
         <section>
           <h1>Oh Hi, React</h1>
         </section>
+        <img src={sword} alt='sword' width='250' />
+        <Avengers />
       </main>
-      <Avengers />
     </>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = (env) => {
   let liveReolad = env.production
     ? { mode: 'production', target: 'browserslist' }
     : { mode: 'development', target: 'web' };
   return {
     ...liveReolad,
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: 'images/[hash][ext][query]',
+    },
     module: {
       rules: [
         {
@@ -15,8 +23,18 @@ module.exports = (env) => {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset',
+        },
       ],
     },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
     resolve: {
       extensions: ['.js', '.jsx'],
     },
